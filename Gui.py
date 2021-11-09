8e7ee566879d825ace825b543a206ae0bf482d48
AridwanW <alwandiaridwan@gmail.com>
2021-11-09 17:53:03 +0700

    add: Gui file task for Kurobu

---
+++ Gui.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import cv2
+
+'''
+    1. Buatlah GUI seperti pada program MK6!! (Tugas Duta)
+    syarat:
+    - tanpa program dronekit
+    - bisa diterapkan multithreading
+    - dibuat dengan metode OOP
+    - jangan persis dengan GUI program MK6
+    - GUI program MK6 hanya sebagai referensi
+'''
